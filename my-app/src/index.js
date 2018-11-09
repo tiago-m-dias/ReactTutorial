ec54316bb68d06c3c1f8e5163fff504d19ab62f9
Tiago Dias <freshman21@gmail.com>
2018-11-09 11:47:10 +0000

    feat: Board render function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -14,28 +14,32 @@ class Board extends React.Component {
 
 
   renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    return <Square key={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+  }
+
+
+  renderBoard() {
+    let boardRows = [];
+    let squareCount = 0;
+    
+    for (let i = 0; i < 3; i++){
+      let children = [];
+
+      for (let j = 0; j < 3; j++) {
+        children.push(this.renderSquare(squareCount));
+        squareCount++;
+      }
+      boardRows.push(<div key={i} className="board-row">{children}</div>)
+    }
+
+    return boardRows;
   }
 
 
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.renderBoard()}
       </div>
     );
   }
